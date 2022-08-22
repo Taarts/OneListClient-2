385192afe8b6ae32ce31bb857ee17716a4654457
Tricia Amheiser <tricia@taarts.net>
2022-08-22 11:09:06 -0400

    item.cs added terniary stmt. used "args" to access API (not working) added spelling to settings

--- Program.cs
+++ Program.cs
@@ -1,27 +1,42 @@
 ﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace OneListClient
 {
     class Program
     {
+        //          *void* usually goes here
+        //           v
         static async Task Main(string[] args)
         {
+            var token = args[0];
+
             var client = new HttpClient();
 
-            var responseBodyAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=cohort24");
+            var responseBodyAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token={token}"); /*-- when "dotnet run" qualify the token with "cohort24" --*/
             // describes the <i> shape</i> of the data
             // (array in JSON => List, Object in JSON => Item)
             //                                    v             v   v
             var items = await JsonSerializer.DeserializeAsync<List<item>>(responseBodyAsStream);
-
+            //  table headers **********************************************************
+            var table = new ConsoleTable("Description", "Created At", "Completed");
+            //  table headers **********************************************************
+            // C# normalcy 
             foreach (var item in items)
             {
-                Console.WriteLine($"the task {item.Text} was created on {item.CreatedAt} and has a completion of {item.CompletedStatus}");
+                // except...
+                // table content **********************************************************
+                table.AddRow(item.Text, item.CreatedAt, item.CompletedStatus);
+                //  table content **********************************************************
+
+                // instead of the "Console.Writeline" below...
+                // Console.WriteLine($"the task {item.Text} was created on {item.CreatedAt} and has a completion of {item.CompletedStatus}");
                 //          /\ coded custom property
+                table.Write();
             }
         }
     }
