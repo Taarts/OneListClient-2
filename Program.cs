9e078dd40c904046fa2785ba614cf67908d3162a
Tricia Amheiser <tricia@taarts.net>
2022-08-22 11:46:51 -0400

    This runs but produces no table when asked in the menu logic... Worked before...

--- Program.cs
+++ Program.cs
@@ -9,15 +9,16 @@ namespace OneListClient
 {
     class Program
     {
-        //          *void* usually goes here
-        //           v
-        static async Task Main(string[] args)
-        {
-            var token = args[0];
 
+        static async Task ShowAllItems(string token)
+        {
             var client = new HttpClient();
+            // var url = $"https://one-list-api.herokuapp.com/items?access_token={token}";
 
             var responseBodyAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token={token}"); /*-- when "dotnet run" qualify the token with "cohort24" --*/
+
+            // var responseBodyAsStream = await client.GetStreamAsync(url);
+
             // describes the <i> shape</i> of the data
             // (array in JSON => List, Object in JSON => Item)
             //                                    v             v   v
@@ -37,6 +38,44 @@ namespace OneListClient
                 // Console.WriteLine($"the task {item.Text} was created on {item.CreatedAt} and has a completion of {item.CompletedStatus}");
                 //          /\ coded custom property
                 table.Write();
+            }
+        }
+        //          *void* usually goes here
+        //           v
+        static async Task Main(string[] args)
+        {
+            var token = "";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("What list would you like? ");
+                token = Console.ReadLine();
+            }
+            else
+            {
+                token = args[0];
+            }
+
+            var keepGoing = true;
+            while (keepGoing)
+            {
+                Console.Clear();
+                Console.Write("Get (A)ll todo, or (Q)uit: ");
+                var choice = Console.ReadLine().ToUpper();
+                switch (choice)
+                {
+                    case "Q":
+                        keepGoing = false;
+                        break;
+                    case "A":
+                        // have to await method when using async
+                        await ShowAllItems(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
     }
